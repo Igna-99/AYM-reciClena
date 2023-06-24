26e6f761049e6aaa3d8df144debefc0b61c01926
igna-99 <ignaciovarela7765@gmail.com>
2023-06-24 18:27:40 -0300

    store y ruteo

---
+++ src/components/views/home.vue
@@ -0,0 +1,36 @@
+<script>
+import { superStore } from './components/store/superStore';
+
+export default {
+  data() {
+    return {
+      store: superStore()
+    }
+  },
+  methods: {
+
+
+  },
+  async created() {
+    let aux = await this.store.logIn("email 1","clave 1")
+
+    console.log(aux)
+
+  } 
+
+}
+</script>
+
+
+<template>
+
+<div>
+  123213123123123
+</div>
+
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/views/registro.vue
@@ -0,0 +1,29 @@
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+
+
+  }
+}
+
+
+
+</script>
+
+
+<template>
+
+  soy el registro
+
+
+</template>
+
+
+<style scoped>
+
+</style>
